5e15ef771934a9ff1ed6ca0d0137f99edee0a6d0
Larry Arrington <larryarrington@pursuit.org>
2023-04-26 12:57:59 -0400

    liftingstates

--- src/Components/Attendee.js
+++ src/Components/Attendee.js
@@ -1,3 +1,54 @@
-export default function Attendee() {
-  return;
+import { useState } from "react";
+
+export default function Attendee({ attendee, events, setEvents }) {
+  const [attendance, setAttendance] = useState(attendee.attendance);
+  const [event, setEvent] = useState();
+
+  function updateEventAttendance(eventId, attendeeId) {
+    console.log("hey");
+    const eventArray = [...events];
+    const eventIndex = eventArray.findIndex((event) => eventId === event.id);
+    setEvent({ ...eventArray[eventIndex] });
+    const personIndex = event.people.findIndex(
+      (person) => person.id === attendeeId
+    );
+    const peopleArray = [...event.people];
+    setAttendance(peopleArray[personIndex].attendance);
+    setAttendance(!peopleArray[personIndex].attendance);
+    eventArray[eventIndex] = event;
+    setEvents(eventArray);
+  }
+
+  return (
+    <>
+      <div key={attendee.id} className="attendee">
+        <p>
+          <img src={attendee.avatar} alt={attendee.firstName} />
+          {"   "}
+          <span>
+            {" "}
+            {attendee.firstName} {attendee.lastName}{" "}
+          </span>
+        </p>
+        <p>
+          <button
+            className="clickable"
+            onClick={() =>
+              updateEventAttendance(
+                // event.id,
+                attendee.id
+              )
+            }
+          >
+            Attending:
+          </button>
+          <span>{attendee.attendence ? "✅" : "❌"}</span>
+        </p>
+
+        <p>
+          <span>Note:</span> {attendee.note}
+        </p>
+      </div>
+    </>
+  );
 }
--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,30 @@
-export default function Attendees() {
-  return;
+import Attendee from "./Attendee";
+import { useState } from "react";
+
+export default function Attendees({ attendees }, { updateEventAttendance }) {
+  //, {attendees}, {setEvents}
+  console.log("hello");
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+
+  return (
+    <>
+      <button onClick={toggleEventAttendees}>
+        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+      </button>
+      {showAttendees ? (
+        <div className="attendees">
+          {attendees.map((attendee, index) => (
+            <Attendee
+              attendee={attendee}
+              updateEventAttendance={updateEventAttendance}
+            />
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
 }
