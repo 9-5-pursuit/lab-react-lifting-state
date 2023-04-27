e8522973f4b7f0950e81369529360653c7e1d130
jeremytjimenez <jeremytjimenez@gmail.com>
2023-04-27 00:00:07 -0400

    bug fixed and elements separated into components

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,20 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+
+export default function Event({ attendees, showAttendees, event, updateEventAttendance, toggleEventAttendees }) {
+  return (
+    <>
+      <li key={event.id}>
+        <img src={event.eventImage} alt={event.name} />
+        <h5>
+          {event.name} {event.eventType}
+        </h5>
+        <br />
+        <span>Organized by: {event.organizer} </span>
+        <br />
+        <>
+          <Attendees attendees={attendees} showAttendees={showAttendees} event={event} updateEventAttendance={updateEventAttendance} toggleEventAttendees={toggleEventAttendees} />
+        </>
+      </li>
+    </>
+  );
 }
