0ca8034202e231ac1b203aa968f6a66c4decc6b6
Karma Ghale <karmaghale@Karmas-Air-2.cable.rcn.com>
2023-04-28 10:08:18 -0400

    Done

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,21 @@
-export default function Event() {
-  return;
+
+import Attendees from "./Attendees"
+export default function Event({event, attendees,updateEventAttendance,Attendees}) {
+  
+  return(
+    <li className="event" key={event.id}>
+    <img src={event.eventImage} alt={event.name} />
+    <h5>
+      {event.name} {event.eventType}
+    </h5>
+    <br />
+    <span>Organized by: {event.organizer} </span>
+    <br />
+    <Attendees
+      updateEventAttendance={updateEventAttendance}
+      event={event}
+      attendees={attendees}
+    />
+  </li>
+ )
 }
