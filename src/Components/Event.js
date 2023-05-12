018c79a368bf37829c46ce90db494376bd2a3166
Esmeralda Gonzalez Rojo <esmeralda@Esmeraldas-Air-3208.lan>
2023-05-12 13:32:09 -0400

    done

--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,31 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees";
+import Header from "./Header";
+
+export default function Event({
+  event,
+  // toggleEventAttendees,
+  // showAttendees,
+  attendees,
+  updateEventAttendance,
+}) {
+  return (
+    <>
+      <li key={event.id}>
+        <img src={event.eventImage} alt={event.name} />
+        <h5>
+          {event.name} {event.eventType}
+        </h5>
+        <br />
+        <span>Organized by: {event.organizer} </span>
+        <br />
+        <Attendees
+          event={event}
+          // toggleEventAttendees={toggleEventAttendees}
+          // showAttendees={showAttendees}
+          attendees={attendees}
+          updateEventAttendance={updateEventAttendance}
+        />
+      </li>
+    </>
+  );
 }
