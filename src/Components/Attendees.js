441d55457fb04fe7ae5c2cc088da65de26a346b9
michael reyes <draftiercrayon4@gmail.com>
2023-06-09 13:17:03 -0400

    lab complete

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,21 @@
-export default function Attendees() {
-  return;
+import React from "react";
+import Attendee from "./Attendee";
+
+function Attendees({ event, updateEventAttendance }) {
+  const { people: attendees } = event;
+
+  return (
+    <div className="attendees">
+      {attendees.map((attendee, index) => (
+        <Attendee
+          key={attendee.id}
+          attendee={attendee}
+          updateEventAttendance={updateEventAttendance}
+          eventId={event.id}
+        />
+      ))}
+    </div>
+  );
 }
+
+export default Attendees;
--- src/Components/Event.js
+++ src/Components/Event.js
@@ -1,3 +1,38 @@
-export default function Event() {
-  return;
+import React, { useState } from "react";
+import Attendees from "./Attendees";
+
+function Event({ event, updateEventAttendance }) {
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+
+  return (
+    <>
+      <li className="event" key={event.id}>
+        <img src={event.eventImage} alt={event.name} />
+        <h5>
+          {event.name} {event.eventType}
+        </h5>
+        <br />
+        <span>Organized by: {event.organizer} </span>
+        <br />
+        <>
+          <button onClick={toggleEventAttendees}>
+            {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+          </button>
+
+          {showAttendees && (
+            <Attendees
+              event={event}
+              updateEventAttendance={updateEventAttendance}
+            />
+          )}
+        </>
+      </li>
+    </>
+  );
 }
+
+export default Event;
