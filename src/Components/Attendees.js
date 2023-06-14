4a0da82a3d4dc6b119368f922a6ff466e6063b6f
Jibon P <jibonp@Jibons-MacBook-Air.local>
2023-06-14 10:30:06 -0400

    done

--- src/Components/Attendees.js
+++ src/Components/Attendees.js
@@ -1,3 +1,33 @@
-export default function Attendees() {
-  return;
+import React from "react";
+
+function Attendees({ attendees, updateEventAttendance }) {
+  return (
+    <div className="attendees">
+      {attendees.map((attendee) => (
+        <div key={attendee.id} className="attendee">
+          <p>
+            <img src={attendee.avatar} alt={attendee.firstName} />
+            {"   "}
+            <span>
+              {attendee.firstName} {attendee.lastName}
+            </span>
+          </p>
+          <p>
+            <button
+              className="clickable"
+              onClick={() => updateEventAttendance(attendee.id)}
+            >
+              Attending:
+            </button>
+            <span>{attendee.attendance ? "✅" : "❌"}</span>
+          </p>
+          <p>
+            <span>Note:</span> {attendee.note}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export default Attendees;
